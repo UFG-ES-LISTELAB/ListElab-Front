b8bf7cba0ec73dc2481a24cd56a956db939f67c9
Rafael Reis <rafareisbr@gmail.com>
2019-10-15 00:34:02 -0300

    add dialog de confirmacao de remoção

--- ListElab/src/app/questions/components/questions-card/questions-card.component.ts
+++ ListElab/src/app/questions/components/questions-card/questions-card.component.ts
@@ -1,7 +1,9 @@
+import Swal from 'sweetalert2';
 import {Component, Input, OnInit, Output} from '@angular/core';
 import {Question} from '../../questions.model';
 import {Router} from '@angular/router';
-import {QUESTOES_CRIAR, QUESTOES_LISTAR} from '../../../shared/constants/routes.contants';
+
+import {QUESTOES_CRIAR} from '../../../shared/constants/routes.contants';
 
 @Component({
   selector: 'app-questions-card',
@@ -24,6 +26,23 @@ export class QuestionsCardComponent implements OnInit {
 
   handleEliminar() {
     console.log('eliminar');
-    this.router.navigate([QUESTOES_LISTAR]);
+    Swal.fire({
+      title: 'Você tem certeza?',
+      text: 'A operação não poderá ser revertida!',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Removida!',
+          'Sua questão foi removida.',
+          'success'
+        );
+      }
+    });
   }
 }
