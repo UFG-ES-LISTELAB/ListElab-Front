caf535c300735f89d41d32e0a0535321766d5c92
lucasnshunter <lucasalemao.ns@gmail.com>
2019-10-22 01:18:52 -0300

    login funcional

--- ListElab/src/app/app.module.ts
+++ ListElab/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import {MatIconModule, MatListModule, MatSidenavModule} from '@angular/material';
 import {InterceptorModule} from './shared/http-interceptor/interceptor.module';
 
@@ -19,7 +19,7 @@ import {InterceptorModule} from './shared/http-interceptor/interceptor.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-
+    FormsModule,
     InterceptorModule,
     MatSidenavModule,
     MatListModule,
--- ListElab/src/app/login/login.component.ts
+++ ListElab/src/app/login/login.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+  } from '@angular/forms';
+  import {LoginService} from "./login.service"
+import { Login } from "./login.model"
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login: Login;
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private loginHttp: LoginService) {}
 
   ngOnInit() {
+    this.login = { email: " ", password: " " };
+    this.loginForm = this.fb.group({
+      login: this.login.email,
+      password: this.login.password
+    });
+    //console.log(this)
+  }
+
+  submitted() {
+    console.log("this.questionForm.value", this.loginForm.value)
+    let sendLogin = {
+      email: this.loginForm.value.login,
+      password: this.loginForm.value.password
+    }
+    this.loginHttp.login(sendLogin)
+      .subscribe(
+        data => console.log(data),
+                      error => console.log(error)
+    )
   }
 
 }
